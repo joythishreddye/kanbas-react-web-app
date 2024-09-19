0cd7df4464cb9a43b5f9821feda247e9fb8081b7
¨joythishreddye¨ <evuri.j@northeastern.edu>
2024-09-19 11:54:33 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Joythish Reddy Evuri</h1>
+          <p>Section 3</p>
+          <ul>
+            <li><a href="/labs/lab1">Lab 1</a></li>
+            <li><a href="/labs/lab2">Lab 2</a></li>
+            <li><a href="/labs/lab3">Lab 3</a></li>
+          </ul>
+          <a href="/kanbas">Kanbas Application</a>
+          <p><a href="https://github.com/your-repo-link">Source Code Repository</a></p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
